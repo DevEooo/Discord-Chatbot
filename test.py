4025d401ddf0b6d2c5c84a42e32b95de1dbcb81e
DevEooo <heralddpanji@gmail.com>
2026-06-20 19:38:54 +0700

    Initializing Project + Test

---
+++ test.py
@@ -0,0 +1,26 @@
+import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+secret = os.getenv('discord_api')
+
+intents = discord.Intents.default()
+intents.message_content = True 
+
+client = discord.Client(intents=intents)
+
+@client.event 
+async def on_ready():
+    print(f"Attempted logged in as: {client.user}")
+    
+@client.event
+async def on_message(message):
+    if message.author == client.user: 
+        return 
+    
+    if message.content.startswith('hello'):
+        await message.channel.send('Hello')
+
+client.run(secret)
