bd54b166d4ae2a7165e43085b7bfddce1958ff2b
DevEooo <heralddpanji@gmail.com>
2026-06-26 02:04:15 +0700

    Configure a chatbot prompt and implement it on service code.

--- src/service/gemini_service.py
+++ src/service/gemini_service.py
@@ -1,16 +1,29 @@
 import os
 from dotenv import load_dotenv
-from google import genai
+from google import genai 
 from src.utils.chunks import split_into_chunks
 
 load_dotenv()
+
+base_directory = os.path.dirname(os.path.abspath(__file__))
+prompt_path = os.path.join(base_directory, "prompts", "prompt.txt")
+
+try:
+    with open(prompt_path, "r", encoding="utf-8") as file:
+        prompt_instruction = file.read().strip()
+except FileNotFoundError:
+    print("Prompt.txt is missing.")
+    
 ai_client = genai.Client(api_key=os.getenv('llm_credential'))
 
-async def get_response(prompt: str) -> list[str]:
+async def get_response(contents: list) -> list[str]:
     try:
         response = ai_client.models.generate_content(
             model='gemini-2.5-flash',
-            contents=prompt,
+            contents=contents,
+            config=genai.types.GenerateContentConfig(
+                system_instruction = prompt_instruction
+            )
         )
         
         return split_into_chunks(response.text)
