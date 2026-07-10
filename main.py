0a5d346e902f1bfeec0581c2a1fec276e9c78492
DevEooo <heralddpanji@gmail.com>
2026-07-11 05:34:41 +0700

    Updating main.py logic where app runs at first  before conditional statements.

--- main.py
+++ main.py
@@ -8,9 +8,13 @@ secret = os.getenv('discord_token')
 llm = os.getenv('llm_api')
 
 if __name__ == "__main__":
-    if secret:
-        client.run(secret)
-    if not secret or llm:
-        raise SystemExit("[ERROR]: Discord / LLM token is missing.")
+    if not llm:
+        raise SystemExit("[ERROR]: LLM token is missing.")
+    if not secret:
+        raise SystemExit("[ERROR]: Discord token is missing.")
+    if not secret and llm:
+        raise SystemExit("[ERROR]: Both Discord and LLM token are missing.")
+    
+    client.run(secret)
     
         
